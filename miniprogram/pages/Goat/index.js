3e0b9f554244b05b17e6861205b5d28aa09b5506
周翀 <929987204@qq.com>
2022-10-16 13:52:06 +0800

    修复了时间差计算的bug

--- miniprogram/pages/Goat/index.js
+++ miniprogram/pages/Goat/index.js
@@ -1,6 +1,6 @@
 // pages/Goat/index.js
-const db=wx.cloud.database().collection("Goat")
-const base=wx.cloud.database()
+const db = wx.cloud.database().collection("Goat")
+const base = wx.cloud.database()
 var util = require('../../utils/utils.js')
 Page({
 
@@ -8,16 +8,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    srcMic:'./videos/mie.mp3',
-    name:'miemie',
-    level:0,
-    ji:0,
-    clean:0,
-    lastji:'',
-    lastclean:'',
-    last:'',
+    srcMic: './videos/mie.mp3',
+    name: 'miemie',
+    level: 0,
+    ji: 0,
+    clean: 0,
+    lastji: '',
+    lastclean: '',
+    last: '',
   },
-  miemie(){
+  miemie() {
     let that = this
     //创建内部 audio 上下文 InnerAudioContext 对象。
     const innerAudioContext = wx.createInnerAudioContext({
@@ -27,137 +27,151 @@ Page({
     innerAudioContext.play(); //播放音频
     console.log(innerAudioContext)
   },
-  addji(e){
-    let that=this;
-    let cha= that.datediff(that.data.lastji);
-    if(cha>0){
+  addji(e) {
+    let that = this;
+    let cha = that.datediff(that.data.lastji);
+    if (cha > 0) {
       let ji = this.data.ji;
-      let date = util.formatTime(new Date(),3);
-      if(ji<=90) ji+=10;
-      else ji=100;
+      let date = util.formatTime(new Date(), 3);
+      if (ji <= 90) ji += 10;
+      else ji = 100;
       this.setData({
-        ji:ji,
-        lastji:date
+        ji: ji,
+        lastji: date
       })
       wx.cloud.callFunction({
-        name:"update",
-        data:{
-          choice:2,
-          setname:'Goat',
-          name:that.data.name,
-          ji:ji,
-          lastji:date
+        name: "update",
+        data: {
+          choice: 2,
+          setname: 'Goat',
+          name: that.data.name,
+          ji: ji,
+          lastji: date
         },
-        success:res=>{
+        success: res => {
           wx.showToast({
             title: '喂养成功',
-            icon: 'success',//图标，支持"success"、"loading"、"none"
-            duration: 1000//设置的持续时间
+            icon: 'success', //图标，支持"success"、"loading"、"none"
+            duration: 1000 //设置的持续时间
           })
         }
       })
-    }
-    else {
+    } else {
       wx.showToast({
         title: '今天已经喂养过了',
-        icon: 'none',//图标，支持"success"、"loading"、"none"
-        duration: 1000//设置的持续时间
+        icon: 'none', //图标，支持"success"、"loading"、"none"
+        duration: 1000 //设置的持续时间
       })
     }
   },
-  addclean(e){
-    let that=this;
+  addclean(e) {
+    let that = this;
     //console.log(that.data.lastclean)
-    let cha= that.datediff(that.data.lastclean);
-    if(cha>0){
+    let cha = that.datediff(that.data.lastclean);
+    if (cha > 0) {
       let clean = this.data.clean;
-      let date = util.formatTime(new Date(),3);
-      if(clean<=90) clean+=10;
-      else clean=100;
+      let date = util.formatTime(new Date(), 3);
+      if (clean <= 90) clean += 10;
+      else clean = 100;
       this.setData({
-        clean:clean,
-        lastclean:date
+        clean: clean,
+        lastclean: date
       })
       wx.cloud.callFunction({
-        name:"update",
-        data:{
-          choice:3,
-          setname:'Goat',
-          name:that.data.name,
-          clean:clean,
-          lastclean:date
+        name: "update",
+        data: {
+          choice: 3,
+          setname: 'Goat',
+          name: that.data.name,
+          clean: clean,
+          lastclean: date
         },
-        success:res=>{
+        success: res => {
           wx.showToast({
             title: '给羊羊洗澡成功',
-            icon: 'success',//图标，支持"success"、"loading"、"none"
-            duration: 1000//设置的持续时间
+            icon: 'success', //图标，支持"success"、"loading"、"none"
+            duration: 1000 //设置的持续时间
           })
         }
       })
-    }
-    else {
+    } else {
       wx.showToast({
         title: '今天已经洗过了',
-        icon: 'none',//图标，支持"success"、"loading"、"none"
-        duration: 1000//设置的持续时间
+        icon: 'none', //图标，支持"success"、"loading"、"none"
+        duration: 1000 //设置的持续时间
       })
     }
   },
   //计算时间差
-  datediff(end){
-    let today = new Date();	
+  datediff(end) {
+    let today = util.formatTime(new Date(), 3);
     let days;
-	end = new Date(end);
-	if(end > today){
-		days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24);
-	}else{
-		days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24); // 如果不限制对比时间和当前时间大小可以不用if
-	}	
-	return days;
+    end = new Date(end);
+    today = new Date(today)
+    if (end > today) {
+      days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24);
+    } else {
+      days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24); // 如果不限制对比时间和当前时间大小可以不用if
+    }
+    return days;
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let that=this
+    let that = this
     wx.cloud.callFunction({
-      name:"getList",
-      data:{
-        list:'Goat'
+      name: "getList",
+      data: {
+        list: 'Goat'
       },
-      success:res=>{
+      success: res => {
         //console.log(res.result.data[0])
-        let {ji,clean,level,lastji,lastclean,last}=res.result.data[0]
+        let {
+          ji,
+          clean,
+          level,
+          lastji,
+          lastclean,
+          last
+        } = res.result.data[0]
         // let jicha = this.datediff(lastji)
         // console.log(jicha,ji)
         // let cleancha = this.datediff(lastclean)
         let cha = this.datediff(last)
-        let date = util.formatTime(new Date(),3);
-        if(cha>0){
+        let date = util.formatTime(new Date(), 3);
+        if (cha > 0) {
           let temp = cha;
-          for(temp;temp>0;temp--){
-            if(ji>0) ji-=10;
-            if(clean>0) clean-=10;
+          for (temp; temp > 0; temp--) {
+            if (ji > 0) ji -= 10;
+            if (clean > 0) clean -= 10;
           }
         }
         //console.log(cha)
-        if(cha>0&&ji>0&&clean>0) {level++;console.log(level)}
+        if (cha > 0 && ji > 0 && clean > 0) {
+          level++;
+          console.log(level)
+        }
         wx.cloud.callFunction({
-          name:"update",
-          data:{
-            choice:4,
-            setname:'Goat',
-            name:that.data.name,
-            clean:clean,
-            ji:ji,
-            last:date,
-            level:level
+          name: "update",
+          data: {
+            choice: 4,
+            setname: 'Goat',
+            name: that.data.name,
+            clean: clean,
+            ji: ji,
+            last: date,
+            level: level
           },
-          success:res=>{}
+          success: res => {}
         })
         that.setData({
-          ji,clean,level,last,lastclean,lastji
+          ji,
+          clean,
+          level,
+          last,
+          lastclean,
+          lastji
         })
         console.log(this.data.last)
       }
--- miniprogram/pages/MainPage/index.js
+++ miniprogram/pages/MainPage/index.js
@@ -1,19 +1,20 @@
 /* Main page of the app */
+var util = require('../../utils/utils.js')
 Page({
     data: {
         creditA: 0,
         creditB: 0,
-        day:0,
-        hour:0,
-        minute:0,
-        second:0,
-        millisecond:0,
+        day: 0,
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
         timecount: '00:00:00',
         userA: '',
         userB: '',
     },
 
-    async onShow(){
+    async onShow() {
         this.getCreditA()
         this.getCreditB()
         this.setData({
@@ -22,36 +23,49 @@ Page({
         })
     },
 
-    getCreditA(){
-        wx.cloud.callFunction({name: 'getElementByOpenId', data: {list: getApp().globalData.collectionUserList, _openid: getApp().globalData._openidA}})
-        .then(res => {
-          this.setData({creditA: res.result.data[0].credit})
-        })
+    getCreditA() {
+        wx.cloud.callFunction({
+                name: 'getElementByOpenId',
+                data: {
+                    list: getApp().globalData.collectionUserList,
+                    _openid: getApp().globalData._openidA
+                }
+            })
+            .then(res => {
+                this.setData({
+                    creditA: res.result.data[0].credit
+                })
+            })
     },
-    
-    getCreditB(){
-        wx.cloud.callFunction({name: 'getElementByOpenId', data: {list: getApp().globalData.collectionUserList, _openid: getApp().globalData._openidB}})
-        .then(res => {
-            this.setData({creditB: res.result.data[0].credit})
-        })
+
+    getCreditB() {
+        wx.cloud.callFunction({
+                name: 'getElementByOpenId',
+                data: {
+                    list: getApp().globalData.collectionUserList,
+                    _openid: getApp().globalData._openidB
+                }
+            })
+            .then(res => {
+                this.setData({
+                    creditB: res.result.data[0].credit
+                })
+            })
     },
-    topasswd(){
+    topasswd() {
         wx.showActionSheet({
             itemList: ['zc', 'qk'],
             success: function (res) {
                 if (!res.cancel) {
-                console.log(res.tapIndex)//点击选择按钮，打印
+                    console.log(res.tapIndex) //点击选择按钮，打印
                 }
-                if(res.tapIndex==0)
-                {
+                if (res.tapIndex == 0) {
                     wx.navigateTo({
-                      url: '../Password/index?user=zc',
+                        url: '../Password/index?user=zc',
                     })
-                }
-                else if(res.tapIndex==1)
-                {
+                } else if (res.tapIndex == 1) {
                     wx.navigateTo({
-                      url: '../Password/index?user=qk',
+                        url: '../Password/index?user=qk',
                     })
                 }
             }
@@ -63,131 +77,131 @@ Page({
         //   complete: (res) => {},
         // })
     },
-    toMessage(){
+    toMessage() {
         wx.navigateTo({
-          url: '../Message/index',
+            url: '../Message/index',
         })
     },
-    tophoto(){
+    tophoto() {
         wx.navigateTo({
             url: '../Photo/index',
-          })
-    },
-    tobeiwang(){
-        wx.navigateTo({
-            url: '../BeiWang/index',
-          })
-    },
-    toyima(){
-        wx.navigateTo({
-            url: '../Yima/index',
-          })
-    },
-    togoat(){
-        wx.navigateTo({
-            url: '../Goat/index',
-          })
-    },
-    toabout(){
-        wx.navigateTo({
-          url: '../About/index',
         })
     },
-    tojizhang(){
+    tobeiwang() {
+        wx.navigateTo({
+            url: '../BeiWang/index',
+        })
+    },
+    toyima() {
+        wx.navigateTo({
+            url: '../Yima/index',
+        })
+    },
+    togoat() {
+        wx.navigateTo({
+            url: '../Goat/index',
+        })
+    },
+    toabout() {
+        wx.navigateTo({
+            url: '../About/index',
+        })
+    },
+    tojizhang() {
         wx.showActionSheet({
             itemList: ['zc', 'qk'],
             success: function (res) {
                 if (!res.cancel) {
-                console.log(res.tapIndex)//点击选择按钮，打印
+                    console.log(res.tapIndex) //点击选择按钮，打印
                 }
-                if(res.tapIndex==0)
-                {
+                if (res.tapIndex == 0) {
                     wx.navigateTo({
-                      url: '../Jizhang/index?user=zc',
+                        url: '../Jizhang/index?user=zc',
                     })
-                }
-                else if(res.tapIndex==1)
-                {
+                } else if (res.tapIndex == 1) {
                     wx.navigateTo({
-                      url: '../Jizhang/index?user=qk',
+                        url: '../Jizhang/index?user=qk',
                     })
                 }
             }
         })
     },
-      /**
-   * 生命周期函数--监听页面加载
-   */
-  datediff(end){
-    let today = new Date();	
-    let days;
-	end = new Date(end);
-	if(end > today){
-		days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24);
-	}else{
-		days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24); // 如果不限制对比时间和当前时间大小可以不用if
-	}	
-	return days;
-  },
-  timer(){
-      let day=this.datediff('2022-02-14')
-      let date = new Date();
-      this.setData({
-          day:day,
-          hour:date.getHours(),
-          minute:date.getMinutes(),
-          second:date.getSeconds()
-      })
-  },
-  onLoad: function (options) {
-    var that=this
-    setInterval(function () { that.timer() }, 100);
-},
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    datediff(end) {
+        let today = util.formatTime(new Date(), 3);
+        let days;
+        end = new Date(end);
+        today = new Date(today)
+        if (end > today) {
+            days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24);
+        } else {
+            days = parseInt(Math.abs(end - today) / 1000 / 60 / 60 / 24); // 如果不限制对比时间和当前时间大小可以不用if
+        }
+        return days;
+    },
+    timer() {
+        let day = this.datediff('2022-02-14')
+        let date = new Date();
+        this.setData({
+            day: day,
+            hour: date.getHours(),
+            minute: date.getMinutes(),
+            second: date.getSeconds()
+        })
+    },
+    onLoad: function (options) {
+        var that = this
+        setInterval(function () {
+            that.timer()
+        }, 100);
+    },
 
-/**
- * 生命周期函数--监听页面初次渲染完成
- */
-onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-},
+    },
 
-/**
- * 生命周期函数--监听页面显示
- */
+    /**
+     * 生命周期函数--监听页面显示
+     */
 
 
-/**
- * 生命周期函数--监听页面隐藏
- */
-onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-},
+    },
 
-/**
- * 生命周期函数--监听页面卸载
- */
-onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-},
+    },
 
-/**
- * 页面相关事件处理函数--监听用户下拉动作
- */
-onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-},
+    },
 
-/**
- * 页面上拉触底事件的处理函数
- */
-onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-},
+    },
 
-/**
- * 用户点击右上角分享
- */
-onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-}
+    }
 })
